5b7b8a2ab837cfc5497c8ac28633ee3fda053297
Sergei <Sergeisafr@mail.ru>
2022-08-21 17:56:10 +0500

    Задача 6 выдаёт является ли число чётным

---
+++ Задача_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int b = number1 % 2;
+if (b == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
