ea24f48522bd83dbe4589b535bd6ecbc4736d7e5
Sergei <Sergeisafr@mail.ru>
2022-08-21 18:01:37 +0500

    Решение Задачи 8 показывает четные числа от 1 до N

---
+++ Задача_8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int ind = 2;
+while (ind <= num)
+{
+   if (ind % 2 ==0)
+    {
+      Console.Write (ind + ", ");
+     ind = ind + 1; 
+    }
+    else 
+    {
+       Console.Write (ind + ", ");
+    }
+   ind = ind + 1; 
+}
