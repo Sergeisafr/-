ac9e7d9d99b3ecf275d67664e9dd67f3d0fe9a05
Sergei <Sergeisafr@mail.ru>
2022-08-21 17:50:47 +0500

    Решение задачи 4 выдает максимальное из 3 чисел

---
+++ Задача_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max;
+max = number1;
+if (max < number2)
+{
+    max = number2;
+ }
+else 
+ {
+    max = number1;
+ }
+
+if (max < number3)
+ {
+     max = number3;
+ }
+ else max = max;
+Console.Write("max = "+ max);
